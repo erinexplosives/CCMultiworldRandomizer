300a50892e111d4170bb1d6366c8d2cb9842a8b3
erinexplosives <latinjakerin@gmail.com>
2023-10-12 01:26:28 -0400

    removed local dependency for "archipelago.js", copied data.json to its original location, to not conflict with plugin.js

--- bundle.sh
+++ bundle.sh
@@ -4,4 +4,5 @@ version="$(jq -r <ccmod.json .version)";
 filename="CCMultiworldRandomizer-$version.ccmod";
 
 [ -f "$filename" ] && rm "$filename"
+cp data/out/data.json data/data.json
 zip -r "$filename" assets ccmod.json data/data.json plugin.js
